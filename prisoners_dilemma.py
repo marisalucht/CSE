2d9ef16bc267406282ccb365a9fbcdc1a53d46b9
marisalucht <marisalucht@gmail.com>
2015-02-25 08:45:58 -0600

    3 strategies created
    I made players who will switch every round, be stuck in the past, and
    choose their move randomly.

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -226,16 +226,16 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 5:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'switch every round'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
+            elif history[-1]=='c': #Collude last round, betray this round
+                return 'b'
             else:
-                return 'c' #otherwise collude
+                return 'c'
     
     
     
@@ -250,13 +250,13 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 6:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'stuck in the past'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
+            if len(opponent_history)==0 or len(opponent_history)==1 : #It's the first two rounds: collude
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
+            elif history[len(opponent_history)-2]=='c' and opponent_history[len(opponent_history)-2]=='b':
                 return 'b' # betray is they were severely punished last time
             else:
                 return 'c' #otherwise collude
@@ -276,16 +276,15 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 7:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'random'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
+            if random.random()<0.5: #randomly choose action
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were sucker last time
             else:
-                return 'c' #otherwise collude
+                return 'b' #otherwise collude
+
 
 
 
